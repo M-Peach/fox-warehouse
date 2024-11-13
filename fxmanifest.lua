5fe23e52d56cadb793095532b041f63422f3e940
M-Peach <92068068+M-Peach@users.noreply.github.com>
2024-11-13 14:21:31 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Foxtrot'
+description 'Warehouse Robbery Script'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+shared_scripts {
+    'config.lua'
+}
+
+dependencies {
+    'qb-core',
+    'qb-target',
+    'numbers'  
+}
